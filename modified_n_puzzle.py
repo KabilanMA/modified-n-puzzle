c16e2cfc716c8cb2d126c00a0da0e2324032bc16
KabilanMA <kabilanen@gmail.com>
2022-10-19 18:23:17 +0530

    Completed

--- modified_n_puzzle.py
+++ modified_n_puzzle.py
@@ -1,5 +1,5 @@
-import copy
 import random
+import copy
 
 def printPretty(array2D):
     for i in range(len(array2D)):
@@ -16,87 +16,27 @@ def accept(filename):
     return output
 
             
-def save(output, sol_found, output_file_name):
-    if sol_found:
-        temp = []
-        for move in output:
-            temp.append(str((int(move[0]), str(move[1]))))
-        s = ", ".join(temp)
-        print(s)
-        with open(output_file_name, 'w') as f:
-            f.write(s)
-    else:
-        with open(output_file_name, 'w') as f:
-            f.write(output)
-
-def evalH(start, goal, heu='manhattan', empty='-'):
-    def evalH1(start, goal, empty):
-        #misplaced tiles
-        # move only to the adjacent slot with only no diagonal movement
-        h = 0
-        for i in range(len(start)):
-            for j in range(len(start)):
-                if start[i][j] != goal[i][j] and start[i][j] != empty:
-                    h+=1
-        return h
-    
-    def evalH2(start, goal, empty):
-        # Total Manhattan distance
-        h = 0
-        for i in range(len(start)):
-            for j in range(len(start)):
-                start_val = start[i][j]
-                if start_val != empty:
-                    got = False
-                    for gi in range(len(goal)):
-                        for gj in range(len(goal)):
-                            if start_val == goal[gi][gj]:
-                                h += abs(gi - i) + abs(gj - j)
-                                got = True
-                                break 
-                        if got:
-                            break
-        return h
-    
-    if heu == 'manhattan':
-        return evalH2(start,goal, empty)
-    else:
-        return evalH1(start, goal, empty)
-    
-
-def evalF(start_node, goal, heu='manhattan'):
-    """f = h + g
-    
-    Keyword arguments:
-    start_node -- start_node is a Node object
-    goal -- goal is a List
-    """
-    
-    f = evalH(start_node.data, goal, heu) + start_node.level
-    return f
-
-# @jit(target_backend='cuda') 
-def belongs(node, list_of_node, heu='misplaced'):
-    for single_node in list_of_node:
-        if evalH(node.data, single_node.data, heu) == 0:
-            return True
-    return False
-
-
 class Node:
-
-    def __init__(self, data, level, f, parent=None, movement=None):
-        #level=g, f = g+h
-        self.parent = parent
+    
+    def __init__(self, data, parent, level):
         self.data = data
-        self.level = level
-        self.f = f
-        self.movement = movement
-
+        self.parent = parent
+        self.level = level # level = g
+    
+    def findEmpty(self, struct=[], x='-'):
+        if not struct:
+            struct = self.data
+        output = []
+        for i in range(0, len(self.data)):
+            for j in range(0, len(self.data)):
+                if struct[i][j] == x:
+                    output.append((i,j))
+        return output
+            
     def getChildren(self):
         empty_slots = self.findEmpty(self.data)
         children = []
-        # print(empty_slots)
+        
         for empty_slot in empty_slots:
             x,y = empty_slot
                 # LEFT      RIGHT    DOWN    UP
@@ -104,24 +44,14 @@ class Node:
             for i, move in enumerate(moves):
                 # print(move)
                 child = self.move(self.data, x, y, move)
-                movement = ['','-']
-                if i == 0:
-                    movement[0] = "Left"
-                if i == 1:
-                    movement[0] = "Right"
-                if i == 2:
-                    movement[0] = "Down"
-                if i == 3:
-                    movement[0] = "Up"
+
                 if child:
-                    val = self.data[move[0]][move[1]]
-                    movement[1] = val
-                    child_node = Node(child, self.level+1, 0, self, movement)
+                    child_node = Node(child, self, self.level+1)
                     children.append(child_node)
         
         # print(children)
         return children
-
+    
     def move(self, struct, x1, y1, next_move):
         try:
             if ((next_move[0] >=0 and next_move[0] < len(self.data)) and (next_move[1]>=0 and next_move[1]<len(self.data))):
@@ -138,88 +68,57 @@ class Node:
         except IndexError:
             return None
         
-    def findEmpty(self, struct=[], x='-'):
-        if not struct:
-            struct = self.data
-        output = []
-        for i in range(0, len(self.data)):
-            for j in range(0, len(self.data)):
-                if struct[i][j] == x:
-                    output.append((i,j))
-        return output
-
-def userFileInputProcess(heu='manhattan'):
-    start = accept(input("Enter the Start Configuration File Name(with .txt extension): "))
-    goal = accept(input("Enter the Goal Configuration File Name(with .txt extension): "))
-    output_file_name = input('Enter output File Name: ')
-           
-    opened = []
-    closed = []
-    start_node = Node(start, 0, 0)
-    start_node.f = evalF(start_node, goal, heu)
-    opened.append(start_node)
-    output_data = []
-    count = 0
-    sol_found = False
-    goal_node = start_node
-    while opened:
-        count += 1
-        if heu=='manhattan' and count>1500:
-            return []
-        if heu=='misplaced' and count>2500:
-            return []
-             
-        opened.sort(key=lambda x:x.f, reverse=True)
-        current_node = opened.pop()
-        closed.append(current_node)
-        if(evalH(current_node.data, goal, heu) == 0):
-            print("Success after {} expand".format(count))
-            goal_node = current_node
-            sol_found = True
-            break
+    def _evalH(self, goal, heu='manhattan', empty='-'):
         
-        best_child = None
-        IN=False
-        
-        for child in current_node.getChildren():
-            
-            isChildBelongsTo = belongs(child, (opened+closed))
-            child.f = evalF(child, goal, heu)
-            if not isChildBelongsTo:
-                opened.append(child)
-            
-            if not best_child:
-                IN = not(isChildBelongsTo)
-                best_child = child
-                
-            elif child.f < best_child.f and belongs(child, closed):
-                IN = not (isChildBelongsTo)
-                best_child = child
+        def evalH1(goal, empty):
+            #misplaced tiles
+            # move only to the adjacent slot with only no diagonal movement
+            h = 0
+            for i in range(len(self.data)):
+                for j in range(len(self.data)):
+                    if self.data[i][j] != goal[i][j] and self.data[i][j] != empty:
+                        h+=1
+            return h
 
-        if not IN:
-            opened.append(best_child)
-    else:
-        print("Faild to find the solution")
+        def evalH2(goal, empty):
+            # Total Manhattan distance
+            h = 0
+            for i in range(len(self.data)):
+                for j in range(len(self.data)):
+                    start_val = self.data[i][j]
+                    if start_val != empty:
+                        got = False
+                        for gi in range(len(goal)):
+                            for gj in range(len(goal)):
+                                if start_val == goal[gi][gj]:
+                                    h += abs(gi - i) + abs(gj - j)
+                                    got = True
+                                    break 
+                            if got:
+                                break
+            return h
 
+        if heu == 'manhattan':
+            self.h =  evalH2(goal, empty)
+        else:
+            self.h = evalH1(goal, empty)
     
-    if sol_found:
-        val = goal_node.movement
-        while val:
-            output_data.append((val[1], val[0]))
-            
-            goal_node = goal_node.parent
-            val = goal_node.movement
-        output_data.reverse() 
-        s = copy.deepcopy(output_data)
-    else:
-        s = "Failed to find the solution"
+    def evalF(self, goal, heu, empty='-'):
+        self._evalH(goal, heu, empty='-')
+        self.f = self.level + self.h
+        return True
+    
+def belongs(checkNode, nodeArray):
+    for inState in nodeArray:
+        if inState.data == checkNode.data:
+            return True, inState
+    
+    return False, None
 
-    save(s, sol_found, output_file_name)
-
-
+ 
 def randomEvaluatorProcess(iteration=100):
     
-    def getRandom(n):        
+    def getRandom(n):
         puzzle = [['-' for j in range(n)] for a in range(n)]
         
         values = [str(x) for x in range(1, n**2 - 1)]
@@ -236,69 +135,9 @@ def randomEvaluatorProcess(iteration=100):
         
         return puzzle
     
-    def getOutput(start, goal, heu):
-        opened = []
-        closed = []
-        start_node = Node(start, 0, 0)
-        start_node.f = evalF(start_node, goal, heu)
-        opened.append(start_node)
-        output_data = []
-        count = 0
-        sol_found = False
-        goal_node = start_node
-        while opened:
-            count += 1 
-            if heu=='manhattan' and count>1500:
-                return []
-            if heu=='misplaced' and count>2500:
-                return []
-            
-            opened.sort(key=lambda x:x.f, reverse=True)
-            current_node = opened.pop()
-            closed.append(current_node)
-
-            if(evalH(current_node.data, goal, heu) == 0):
-                print("Success after {} expand".format(count))
-                goal_node = current_node
-                sol_found = True
-                break
-            
-            best_child = None
-            IN=False
-            
-            for child in current_node.getChildren():
-                
-                isChildBelongsTo = belongs(child, (opened+closed))
-                child.f = evalF(child, goal, heu)
-                if not isChildBelongsTo:
-                    opened.append(child)
-                    
-                if not best_child:
-                    IN = not(isChildBelongsTo)
-                    best_child = child
-                    
-                elif child.f < best_child.f and belongs(child, closed):
-                    IN = not (isChildBelongsTo)
-                    best_child = child
-            
-            if not IN:
-                opened.append(best_child)
-        else:
-            print("Faild to find the solution")
-    
-        
-        if sol_found:
-            val = goal_node.movement
-            while val:
-                output_data.append((val[1], val[0]))
-                goal_node = goal_node.parent
-                val = goal_node.movement
-            output_data.reverse()
-            return output_data
-        else:
-            return []
-    
-    def rearranch(array2d, iteration=random.randrange(20,30)):
+    def rearrange(array2d, iteration=0):
+        iteration = random.randint(10,20)
+        print("Number re-arrangements: {}".format(2*iteration))
         for _ in range(iteration):
             
             empty_slots = []
@@ -319,36 +158,142 @@ def randomEvaluatorProcess(iteration=100):
                 movei = positions[random.randrange(len(positions))]
                 array2d[x][y] = array2d[movei[0]][movei[1]]
                 array2d[movei[0]][movei[1]] = '-'
+                    
+    def getOutput(start, goal, heu):
+        OPEN = []
+        CLOSED = []
+        rootNode = Node(start, None, 0)
+        rootNode.evalF(goal, heu)
+        OPEN.append(rootNode)
+        count = 0
+        while OPEN:
+            count +=1
+            OPEN.sort(key=lambda x:x.f, reverse=False)
+            current_state = OPEN.pop(0)
+            
+            if current_state.data == goal:
+                print("Success after {} expand - {}".format(count-1, heu))
+                return count -1
+
+            CLOSED.append(current_state)
+            
+            for nextState in current_state.getChildren():
+                if belongs(nextState, CLOSED)[0]:
+                    continue
                 
-                   
+                if not (belongs(nextState, OPEN)[0]):
+                    nextState.evalF(goal, heu)
+                    OPEN.append(nextState)
+                else:
+                    openNode = belongs(nextState, OPEN)[1]
+                    if nextState.level < openNode.level:
+                        openNode.level = openNode.level
+                        openNode.parent = nextState.parent
+        else:
+            return 0
+        
+        
     output = []
     for i in range(iteration):
         
         n = random.randint(5,20)
-        # n = 25
         start = getRandom(n)
         # goal = getRandom(n)
         goal = copy.deepcopy(start)
-        rearranch(start)
+        rearrange(start)
         
-        output_manhattan = getOutput(start, goal, 'manhattan')
-        manhattan = 'inf' if len(output_manhattan) == 0 else str(len(output_manhattan))
+        node_expanded_misplaced = getOutput(start, goal, 'misplaced')
         
-        if manhattan !='inf':
-          output_misplaced = getOutput(start, goal, 'misplaced')
-          misplaced = 'inf' if len(output_misplaced) == 0 else str(len(output_misplaced))
-          print((str(misplaced) + '\t' + str(manhattan)))
-          output.append((str(misplaced) + '     -     ' + str(manhattan)))
+        if node_expanded_misplaced != 0:
+          node_expanded_manhattan = getOutput(start, goal, 'manhattan')
+          print((str(node_expanded_misplaced) + '\t' + str(node_expanded_manhattan)))
+          output.append(("   "+str(node_expanded_misplaced) + '     -  ' + str(node_expanded_manhattan)))
 
     writeDoc = '\n'.join(output)
-    writeDoc = 'misplaced   manhattan\n' + writeDoc
+    writeDoc = 'misplaced\tmanhattan\n' + writeDoc
     
     with open('random.txt','w') as f:
         f.write(writeDoc)
+
+def userFileInputProcess(heu='manhattan'):
+    start = accept(input("Enter the Start Configuration File Name(with .txt extension): "))
+    goal = accept(input("Enter the Goal Configuration File Name(with .txt extension): "))
+    output_file_name = input('Enter output File Name: ')
+    
+    
+    def getMove(fromState, toState, empty='-'):
+        n = len(fromState.data)
+        temp = (None, None)
+        for i in range(n):
+            for j in range(n):
+                if fromState.data[i][j] != toState.data[i][j] and fromState.data[i][j] != empty:
+                    
+                    if i-1>=0 and fromState.data[i][j] == toState.data[i-1][j]:
+                        temp = fromState.data[i][j], "Up"
+                    elif i+1 <= n-1 and fromState.data[i][j] == toState.data[i+1][j]:
+                        temp = fromState.data[i][j], "Down"
+                    elif j-1>=0 and fromState.data[i][j] == toState.data[i][j-1]:
+                        temp = fromState.data[i][j], "Left"
+                    elif j+1 <= n-1 and fromState.data[i][j] == toState.data[i][j+1]:
+                        temp = fromState.data[i][j], "Right"
+                    break
+        return str(temp)
+                        
+                    
+    def backTrack(node):
+        path = []
+        while node.parent:
+            path.append(getMove(node.parent, node))
+            node = node.parent
+        path.reverse()
+        return path 
+    
+    
+    def getOutput():       
+        OPEN = []
+        CLOSED = []
+        rootNode = Node(start, None, 0)
+        rootNode.evalF(goal, heu)
+        OPEN.append(rootNode)
+        count = 0
+        while OPEN:
+            count +=1
+            OPEN.sort(key=lambda x:x.f, reverse=False)
+            current_state = OPEN.pop(0)
+            
+            if current_state.data == goal:
+                print("Success after {} expand - {}".format(count-1, heu))
+                return backTrack(current_state)
+
+            CLOSED.append(current_state)
+            
+            for nextState in current_state.getChildren():
+                if belongs(nextState, CLOSED)[0]:
+                    continue
+                
+                if not (belongs(nextState, OPEN)[0]):
+                    nextState.evalF(goal, heu)
+                    OPEN.append(nextState)
+                else:
+                    openNode = belongs(nextState, OPEN)[1]
+                    if nextState.level < openNode.level:
+                        openNode.level = openNode.level
+                        openNode.parent = nextState.parent
+        else:
+            return []
         
-        
-# userFileInputProcess()       
-# randomEvaluatorProcess(iteration=1)            
+    result = getOutput()
+    if len(result)!=0:
+        s = ', '.join(result)
+        print(s)
+        with open(output_file_name, 'w') as f:
+            f.write(s)
+    else:
+        s = "Failed"
+        with open(output_file_name, 'w') as f:
+            f.write(s)
+       
+           
 while True:
     selection = input("\nSelect the option of operation using the integer value.\n1.Solve the n-puzzle problem in from the file.\n2.Solve the n-puzzle problem for random generator.\nAny other key to exit the program.\n\t: ")
     if(selection == '1'):
